209b472b4b5e7b0d16d7bc29027e1de5e510c2c8
troido <troido@protonmail.com>
2023-09-16 18:16:03 +0200

    initial environment

--- code/world.gd
+++ code/world.gd
@@ -2,21 +2,50 @@ extends Node3D
 
 const SPAWN_RANDOM := 5.0
 
+enum Tile {Open, Rock}
+
+var mesh_map = {}
+
+func tile_at(p: Vector2i) -> Tile:
+	if p.length() > 6 || p.length() == 0:
+		return Tile.Rock
+	else:
+		return Tile.Open
+
+func view(p: Vector2i):
+	var old_mesh: Node3D = mesh_map.get(p)
+	if old_mesh:
+		old_mesh.queue_free()
+	var new_mesh: Node3D = mesh_for(tile_at(p))
+	mesh_map[p] = new_mesh
+	new_mesh.position.x = p.x
+	new_mesh.position.z = p.y
+	$Environment.add_child(new_mesh)
+
+func mesh_for(tile: Tile):
+	if tile == Tile.Open:
+		return preload("res://scenes/tiles/ground.tscn").instantiate()
+	elif tile == Tile.Rock:
+		return preload("res://scenes/tiles/rock.tscn").instantiate()
+	assert(false, "unknown tile")
+
 func _ready():
 	# We only need to spawn players on the server.
-	if not multiplayer.is_server():
-		return
+	if multiplayer.is_server():
+		multiplayer.peer_connected.connect(add_player)
+		multiplayer.peer_disconnected.connect(del_player)
 
-	multiplayer.peer_connected.connect(add_player)
-	multiplayer.peer_disconnected.connect(del_player)
+		# Spawn already connected players.
+		for id in multiplayer.get_peers():
+			add_player(id)
 
-	# Spawn already connected players.
-	for id in multiplayer.get_peers():
-		add_player(id)
-
-	# Spawn the local player unless this is a dedicated server export.
-	if not OS.has_feature("dedicated_server"):
-		add_player(1)
+		# Spawn the local player unless this is a dedicated server export.
+		if not OS.has_feature("dedicated_server"):
+			add_player(1)
+	
+	for x in range(-20, 20):
+		for y in range(-20, 20):
+			view(Vector2i(x, y))
 
 
 func _exit_tree():
