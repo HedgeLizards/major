75d824fcf5b1ab3a0e2cc2fbe001d58b1c57856b
Milan Raphaël <MilanTent@live.nl>
2023-09-17 17:30:46 +0200

    SOOOUNDS

--- code/sfx.gd
+++ code/sfx.gd
@@ -1,23 +1,32 @@
 extends Node3D
 
+var canShoot : bool = true
+var cooldown : float = .15
+var timer : float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	test_fire();
+	test_fire(delta);
 	test_drill();
 	test_sound_collect();
 	test_sound_grab();
 	test_sound_rotate();
 	test_sound_place();
 
-func test_fire():
-	if Input.is_action_just_pressed("debug_fire"):
+func test_fire(delta):
+	if Input.is_action_pressed("debug_fire") and canShoot:
 		$Fire.play();
+		canShoot = false;
+		timer = cooldown;
+	
+	if !canShoot:
+		timer -= delta;
+		if timer <= 0:
+			canShoot = true;
 
 func test_drill():
 	if Input.is_action_pressed("debug_drill"):
