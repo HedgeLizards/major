cafbe016cc6680a78ca29fe48512cef6647e7de8
Milan Raphaël <milantent@live.nl>
2023-09-21 22:22:16 +0200

    Added sound effect for shooting

--- code/Gun.gd
+++ code/Gun.gd
@@ -4,6 +4,18 @@ var index = 3
 var solid = false
 var powered = false
 
+var canShoot : bool = true
+var cooldown : float = .15
+var timer : float = 0
+
 func _process(_delta):
-	if get_node("..").is_local() && Input.is_action_just_pressed("shoot"):
+	if get_node("..").is_local() && Input.is_action_pressed("shoot") and canShoot:
 		get_tree().call_group("creators", "shoot", global_position, global_rotation)
+		$SND_FIRE.play();
+		canShoot = false;
+		timer = cooldown;
+	
+	if !canShoot:
+		timer -= _delta;
+		if timer <= 0:
+			canShoot = true;
