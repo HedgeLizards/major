25d89bf2139f4fdbbb73b74124300a234d31474e
Milan Raphaël <milantent@live.nl>
2023-09-22 15:32:32 +0200

    Sound effects are now properly spatialized

--- code/sfx.gd
+++ code/sfx.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-var drilling_count : int = 0
+var drill_count : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,13 +14,13 @@ func play_sound_shoot():
 	$SND_SHOOT.play();
 
 func play_sound_digging():
-	drilling_count += 1
+	drill_count += 1
 	if !$SND_DRILL_LOOP.playing:
 		$SND_DRILL_LOOP.play()
 
 func stop_sound_digging():
-	drilling_count = max(0, drilling_count - 1)
-	if drilling_count == 0 and $SND_DRILL_LOOP.playing:
+	drill_count = max(0, drill_count - 1)
+	if drill_count == 0 and $SND_DRILL_LOOP.playing:
 		$SND_DRILL_LOOP.stop()
 
 func play_sound_hit():
