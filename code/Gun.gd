8a38ce42b9f78a656bcb47a8c6eb7a7a2d8bdad6
Milan Raphaël <milantent@live.nl>
2023-09-24 17:05:36 +0200

    Updated gun

--- code/Gun.gd
+++ code/Gun.gd
@@ -4,28 +4,46 @@ var index = 3
 var solid = false
 var powered = false
 
-var canShoot : bool = true
-var cooldown : float = .05
+var can_shoot : bool = true
+var cooldown : float = .3 # Cooldown between bursts
 var timer : float = 0
+var original_burst_count : int = 3 # Shots per burst
+var burst_count = original_burst_count
+var burst_interval : float = .05 # Time between shots in a burst
+var burst_timer : float = 0
 
 signal gun_shot
 
 func _ready():
 	connect("gun_shot", SFX.play_sound_shoot)
+	$AnimationPlayer.stop()
 
-func _process(_delta):
-	if Input.is_action_pressed("shoot"):
-		$AnimationPlayer.play();
-	else:
-		$AnimationPlayer.stop();
+func _process(delta):
+	# Handle Animation Player
+	#if Input.is_action_pressed("shoot"):
+		
+	#else:
+
+	# Check if the gun can shoot a burst
+	if get_node("..").is_local() && Input.is_action_pressed("shoot") && can_shoot:
+		can_shoot = false
+		timer = cooldown
+		
+	# Handle burst firing
+	if !can_shoot and burst_count > 0:
+		burst_timer += delta
+		if burst_timer >= burst_interval:
+			$AnimationPlayer.stop()
+			get_tree().call_group("creators", "shoot", $GunMesh/BulletSpawn.global_position, global_rotation)
+			emit_signal("gun_shot")
+			burst_count -= 1
+			burst_timer = 0
+			$AnimationPlayer.play()
 	
-	if get_node("..").is_local() && Input.is_action_pressed("shoot") && canShoot:
-		get_tree().call_group("creators", "shoot", $GunMesh/BulletSpawn.global_position, global_rotation)
-		emit_signal("gun_shot")
-		canShoot = false;
-		timer = cooldown;
-	
-	if !canShoot:
-		timer -= _delta;
+	# Reset burst count and handle cooldown
+	if !can_shoot and burst_count <= 0:
+		#$AnimationPlayer.stop()
+		timer -= delta
 		if timer <= 0:
-			canShoot = true;
+			can_shoot = true
+			burst_count = original_burst_count # Reset burst count
--- code/bullet.gd
+++ code/bullet.gd
@@ -1,17 +1,23 @@
 extends Node3D
 
-var speed: float = 40
+var speed: float = 70
+var max_spread : float = 1
+var spread
 
 signal bullet_hit
 
 func _ready():
+	spread = randf_range(-max_spread, max_spread)
 	connect("bullet_hit", SFX.play_sound_hit)
 
 func _on_timer_timeout():
 	queue_free()
 
 func _physics_process(delta):
-	position += Vector3(0, 0, -delta * speed).rotated(Vector3(0, 1, 0), rotation.y)
-	
+	position += Vector3(0, 0, -delta * speed).rotated(Vector3(0, 1, 0), rotation.y + deg_to_rad(spread))
+	# This line is modified to include the spread in bullet's direction.
+
+# This function isn't called anywhere right now, but
+# should ideally be called when colliding with another player or other boundary.
 func hit():
 	emit_signal("bullet_hit")
